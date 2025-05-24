26dcd678a9eb039d8bf71b9fdcbed65f75d951ab
Markxu <51899048+xubiaolin@users.noreply.github.com>
2025-05-24 23:48:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -65,6 +65,7 @@ docker run -d \
     -e WG_ALLOWED_IPS=10.0.8.0/24 \
     -e WG_PERSISTENT_KEEPALIVE=25 -v $(pwd)/data/etc/wireguard:/etc/wireguard \
     -e WG_PORT=$TCP_PORT\
+    -e WG_MTU=1280\
     -p $TCP_PORT:51820/udp \
     -p $TCP_PORT:51821/tcp \
     --cap-add=NET_ADMIN \
